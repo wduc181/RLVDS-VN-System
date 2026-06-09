8ccc09cd32e09e69b9e3ec1895b41585c8cb765f
wduc181 <vvd1811@gmail.com>
2026-06-09 20:07:10 +0700

    Add license plate speed estimator

--- rlvds/tracking/__init__.py
+++ rlvds/tracking/__init__.py
@@ -7,4 +7,9 @@ Multi-object tracking để theo dõi biển số qua các frames.
 Modules:
     - tracker.py: Main tracker implementation
     - track_state.py: Track lifecycle management
+    - speed_estimator.py: License plate speed warning estimates
 """
+
+from rlvds.tracking.speed_estimator import LicensePlateSpeedEstimator, SpeedEstimate
+
+__all__ = ["LicensePlateSpeedEstimator", "SpeedEstimate"]
